b7b341403a9516008c8183e192b7c068576eea70
kiril <kirill.301522@gmail.com>
2024-11-23 16:50:54 +1000

    [FGXI-43](feat) celery

--- core/config/celery.py
+++ core/config/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
@@ -9,3 +10,11 @@ app = Celery('backend')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
+
+
+app.conf.beat_schedule = {
+    'send-event-reminders-every-minute': {
+        'task': 'apps.events.tasks.send_event_reminders',
+        'schedule': crontab(),  # По умолчанию запускается каждую минуту
+    },
+}
